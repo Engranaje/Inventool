6992c0072dd3bd1e4e500d6cb64882518d07e802
Harverbo <37876363+Harverbo@users.noreply.github.com>
2019-10-13 23:55:38 -0400

    Upgraded admin controller

--- private/controllers/admin.php
+++ private/controllers/admin.php
@@ -7,10 +7,19 @@ class Admin extends Controller
     /**
      * Function to call model
      */
-    public function callModel()
+    protected function callModel()
     {
         $this->user = new Functions();
         $this->model = new AdminModel();
+    }
+
+    /** Function to verify if user is admin */
+    protected function verifyAdmin()
+    {
+        if (!$this->user->is_admin()) {
+            header('Location:' . ROOT_URL . '/errors/403.php');
+            return;
+        }
     }
 
     /**
@@ -18,13 +27,13 @@ class Admin extends Controller
      */
     protected function index()
     {
+        // Call admin model
         $this->callModel();
 
-        if ($this->user->is_admin()) {
-            $this->returnView(null, 'main');
-        } else {
-            header('Location:' . ROOT_URL . '/errors/403.php');
-        }
+        // Verify that user is admin
+        $this->verifyAdmin();
+
+        $this->returnView(null, 'main');
     }
 
     /**
@@ -32,74 +41,73 @@ class Admin extends Controller
      */
     protected function user()
     {
+        // Call admin model
         $this->callModel();
 
-        // Access user routes in administration us user is an administrator
-        if ($this->user->is_admin()) {
-            if ($this->secondAction == 'edit') {
-                // Edit user
-                $this->returnView($this->model->edit_user($this->id), 'main');
-            } else if ($this->secondAction == 'update') {
-                // Update user if not itself
-                if ($this->id != $_SESSION['user_id']) {
-                    $this->model->update_user($this->id);
-                } else {
-                    Functions::flash('error', 'No puede actualizar su usuario desde esta página');
-                    header('Location:' . ROOT_URL . '/admin');
-                }
-            } else if ($this->id == 'create' && $this->secondAction == 'index') {
-                // Create user
-                $this->returnCustomView($this->model->create_user(), 'admin','create_user');
-            } else if ($this->id == 'info' && $this->secondAction == 'index') {
-                // Show created user information if it has been created
-                if(isset($_SESSION['user_info'])){
-                    $this->returnCustomView($this->model->user_info(), 'admin','user_info');
-                } else {
-                    header('Location:' . ROOT_URL . '/errors/404.php');
-                }
-            } else if ($this->secondAction == 'delete') {
-                // Delete user if not itself
-                if ($this->id != $_SESSION['user_id']) {
-                    $this->model->delete_user($this->id);
-                } else {
-                    Functions::flash('error', 'No puede eliminar su usuario desde esta página');
-                    header('Location:' . ROOT_URL . '/admin');
-                }
-            } else if ($this->secondAction == 'recover') {
-                // Recover user if not itself
-                if ($this->id != $_SESSION['user_id']) {
-                    $this->model->recover_user($this->id);
-                } else {
-                    Functions::flash('error', 'No puede recuperar su usuario');
-                    header('Location:' . ROOT_URL . '/admin');
-                }
-            } else if ($this->secondAction == 'approve') {
-                // Approve user if not itself
-                if ($this->id != $_SESSION['user_id']) {
-                    $this->model->approve_user($this->id);
-                } else {
-                    Functions::flash('error', 'No puede aprobar su usuario');
-                    header('Location:' . ROOT_URL . '/admin');
-                }
-            } else if ($this->secondAction == 'disapprove') {
-                // Disapprove user if not itself
-                if ($this->id != $_SESSION['user_id']) {
-                    $this->model->disapprove_user($this->id);
-                } else {
-                    Functions::flash('error', 'No puede desaprobar su usuario');
-                    header('Location:' . ROOT_URL . '/admin');
-                }
-            } else if ($this->secondAction == 'destroy') {
-                // Permanently delete user if not itself
-                if ($this->id != $_SESSION['user_id']) {
-                    $this->model->destroy_user($this->id);
-                } else {
-                    Functions::flash('error', 'No puede eliminar su usuario desde esta página');
-                    header('Location:' . ROOT_URL . '/admin');
-                }
+        // Verify that user is admin
+        $this->verifyAdmin();
+
+        if ($this->secondAction == 'edit') {
+            // Edit user
+            $this->returnView($this->model->edit_user($this->id), 'main');
+        } else if ($this->secondAction == 'update') {
+            // Update user if not itself
+            if ($this->id != $_SESSION['user_id']) {
+                $this->model->update_user($this->id);
+            } else {
+                Functions::flash('error', 'No puede actualizar su usuario desde esta página');
+                header('Location:' . ROOT_URL . '/admin');
             }
-        } else {
-            header('Location:' . ROOT_URL . '/errors/403.php');
+        } else if ($this->id == 'create' && $this->secondAction == 'index') {
+            // Create user
+            $this->returnCustomView($this->model->create_user(), 'admin','create_user');
+        } else if ($this->id == 'info' && $this->secondAction == 'index') {
+            // Show created user information if it has been created
+            if(isset($_SESSION['user_info'])){
+                $this->returnCustomView($this->model->user_info(), 'admin','user_info');
+            } else {
+                header('Location:' . ROOT_URL . '/errors/404.php');
+            }
+        } else if ($this->secondAction == 'delete') {
+            // Delete user if not itself
+            if ($this->id != $_SESSION['user_id']) {
+                $this->model->delete_user($this->id);
+            } else {
+                Functions::flash('error', 'No puede eliminar su usuario desde esta página');
+                header('Location:' . ROOT_URL . '/admin');
+            }
+        } else if ($this->secondAction == 'recover') {
+            // Recover user if not itself
+            if ($this->id != $_SESSION['user_id']) {
+                $this->model->recover_user($this->id);
+            } else {
+                Functions::flash('error', 'No puede recuperar su usuario');
+                header('Location:' . ROOT_URL . '/admin');
+            }
+        } else if ($this->secondAction == 'approve') {
+            // Approve user if not itself
+            if ($this->id != $_SESSION['user_id']) {
+                $this->model->approve_user($this->id);
+            } else {
+                Functions::flash('error', 'No puede aprobar su usuario');
+                header('Location:' . ROOT_URL . '/admin');
+            }
+        } else if ($this->secondAction == 'disapprove') {
+            // Disapprove user if not itself
+            if ($this->id != $_SESSION['user_id']) {
+                $this->model->disapprove_user($this->id);
+            } else {
+                Functions::flash('error', 'No puede desaprobar su usuario');
+                header('Location:' . ROOT_URL . '/admin');
+            }
+        } else if ($this->secondAction == 'destroy') {
+            // Permanently delete user if not itself
+            if ($this->id != $_SESSION['user_id']) {
+                $this->model->destroy_user($this->id);
+            } else {
+                Functions::flash('error', 'No puede eliminar su usuario desde esta página');
+                header('Location:' . ROOT_URL . '/admin');
+            }
         }
     }
 
@@ -108,16 +116,16 @@ class Admin extends Controller
      */
     protected function users()
     {
+        // Call admin model
         $this->callModel();
 
-        if ($this->user->is_admin()) {
-            if ($this->id != 'trash') {
-                $this->returnView($this->model->users(), 'main');
-            } else {
-                $this->returnCustomView($this->model->deleted_users(), 'admin', 'trash_users');
-            }
+        // Verify that user is admin
+        $this->verifyAdmin();
+
+        if ($this->id != 'trash') {
+            $this->returnView($this->model->users(), 'main');
         } else {
-            header('Location:' . ROOT_URL . '/errors/403.php');
+            $this->returnCustomView($this->model->deleted_users(), 'admin', 'trash_users');
         }
     }
 
@@ -126,20 +134,20 @@ class Admin extends Controller
      */
     protected function notifications()
     {
+        // Call admin model
         $this->callModel();
 
-        if ($this->user->is_admin()) {
-            if ($this->id != 'notify' && $this->secondAction != 'unnotify') {
-                $this->returnView($this->model->notifications(), 'main');
-            } else {
-                if($this->secondAction == 'unnotify'){
-                    $this->model->unnotify($this->id);
-                }else{
-                    $this->model->notify();
-                }
-            }
+        // Verify that user is admin
+        $this->verifyAdmin();
+
+        if ($this->id != 'notify' && $this->secondAction != 'unnotify') {
+            $this->returnView($this->model->notifications(), 'main');
         } else {
-            header('Location:' . ROOT_URL . '/errors/403.php');
+            if($this->secondAction == 'unnotify'){
+                $this->model->unnotify($this->id);
+            }else{
+                $this->model->notify();
+            }
         }
     }
 
@@ -148,6 +156,7 @@ class Admin extends Controller
      */
     protected function hide_notifications()
     {
+        // Call admin model
         $this->callModel();
 
         $this->model->hide_notifications();
